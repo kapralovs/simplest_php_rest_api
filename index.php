7d42c1269769c51c8794fbb8e9c6239934b36aed
kapralovs <ser-kapralov@yandex.ru>
2022-05-15 15:37:23 +0300

    WIP

--- index.php
+++ index.php
@@ -3,6 +3,16 @@ $myObj->name="John";
 $myObj->age=23;
 $myObj->city="New York";
 
+$d = dir(getcwd());
+
+$a=array();
+
+while (($file = $d->read()) !== false){
+  array_push($a,$file);
+}
+$d->close();
+
+$myObj->files=$a;
 $myJSON=json_encode($myObj);
 
 echo $myJSON;
